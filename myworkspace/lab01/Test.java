9ec6081c43a17b9dc1c4cc6f6454075207fb776a
Robertative <roberta.ettamimi@unitn.it>
2016-03-11 13:28:50 +0100

    test.java

---
+++ myworkspace/lab01/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String args[]){
+
+        System.out.println("Hello World!");
+        System.out.println("roberta");
+        System.out.println("active");
+        System.out.println("impariamo");
+
+
+    }
+}
